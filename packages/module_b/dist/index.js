3ebe0f89971d44b52583d5ca2958c3088d6d70cd
tabluw <luming.wang@rwwa.com.au>
2019-07-22 14:14:41 +0800

    failed to make Ramba works

---
+++ packages/module_b/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var rambda_1 = __importDefault(require("rambda"));
+exports.add = function (x, y) { return rambda_1.default.add(x, y); };
---
+++ packages/module_b/src/index.ts
@@ -0,0 +1,2 @@
+import R from 'rambda'
+export const add = (x:number,y:number) => R.add(x,y)
